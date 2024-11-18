fd9e551174d0d369eec96f26e48c8ea398f64e30
Annaelle <annaellebaiget@g.ucla.edu>
2024-11-18 11:42:00 -0800

    play prompt when user has 0 points

--- front/src/pages/LeaderBoardPage.tsx
+++ front/src/pages/LeaderBoardPage.tsx
@@ -5,6 +5,9 @@ import LeaderBoard from '../components/LeaderBoard';
 import type { User } from '@shared_types';
 import { useAppSelector } from '../redux/hook';
 import FullScreenSpinner from '../components/FullScreenSpinner';
+import { Typography } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
+import PlayPrompt from '../components/PlayPrompt';
 
 function LeaderBoardPage() {
   const [users, setUsers] = useState<User[]>([]);
@@ -12,6 +15,7 @@ function LeaderBoardPage() {
   const api = new ApiSdk();
   const { token } = useAppSelector((state) => state.session);
   const { userInfo } = useAppSelector((state) => state.user);
+  const navigate = useNavigate();
 
   useEffect(() => {
     if (!hasFetched.current) {
@@ -27,7 +31,15 @@ function LeaderBoardPage() {
 
   return (
     <ContainerPage title={'LeaderBoard'}>
-      <LeaderBoard users={users} currentUser={userInfo.username}></LeaderBoard>
+      {userInfo.overallScore <= 0 && (
+        <PlayPrompt onPlay={() => navigate('/play')} />
+      )}
+      {userInfo.overallScore > 0 && (
+        <LeaderBoard
+          users={users}
+          currentUser={userInfo.username}
+        ></LeaderBoard>
+      )}
     </ContainerPage>
   );
 }
