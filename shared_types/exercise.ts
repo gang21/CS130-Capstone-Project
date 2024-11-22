0b62a36a025f35a026b989678a626f10211edb72
Annaelle <annaellebaiget@g.ucla.edu>
2024-11-21 22:40:49 -0800

    add enum with the type

--- shared_types/exercise.ts
+++ shared_types/exercise.ts
@@ -1,8 +1,14 @@
 import { ScamCategory } from './scamCategory';
 import { z } from 'zod';
 
+enum ExerciseType {
+  Email = 'email',
+  Text = 'text',
+  default = '',
+}
+
 export const exerciseSchema = z.object({
-  type: z.string(),
+  type: z.nativeEnum(ExerciseType),
   scam: z.boolean(),
   category: z.nativeEnum(ScamCategory),
   message: z.string(),
